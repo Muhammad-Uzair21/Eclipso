20f19b7ab471ea1031c6b1c68181d73c929804a7
Muhammad Uzair <uzair.jay21@gmail.com>
2025-08-19 18:06:32 +0500

    Head icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Eclipso | Modern chatbot",
   description: "Let's chat",
+  icons: {
+    icon: '/icon.svg'
+  }
 };
 
 export default function RootLayout({
